2b19128e53c1cae7d1f13f7f1971634cba3686c4
SeikokuEco <seikoku.eco@gmail.com>
2023-02-21 17:21:17 -0500

    added some dummy data

--- API/views/DummySQl/students.sql
+++ API/views/DummySQl/students.sql
@@ -1,1 +1,9 @@
 insert into students values ('300023232323', 'Carl', 'Johnson', 'sadfrgvewfwefre@iu.edu');
+
+
+
+
+
+INSERT INTO students VALUES ("3001274639", "Ron", "Cookie", "RonCookieDoughMaster@iu.edu");
+INSERT INTO students VALUES ("3002584560", "Boulder", "Snakey", "SnakeOnABoulder@iu.edu");
+INSERT INTO students VALUES ("3003698741", "Jack", "Feral", "JackIsFeral@iu.edu");
