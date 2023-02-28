e735ed6552c7cf86dfabcc454dc571551cb43fb5
Youngdanthevibegod <danielajagbonna@gmail.com>
2023-02-28 16:37:56 -0500

    creating admin data

---
+++ DummySQl/admin.sql
@@ -0,0 +1,3 @@
+insert into admin values("00000","Dan" ,"Smith" ,"Smithdaniels@gmail.com" ,"cieZniices");
+insert into admin values("00001","Ron" ,"Riley" ,"Rileyronsz@gmail.com" ,"iwnicnWDini");
+insert into admin values("00002","Lisa" ,"Brown" ,"Brown1lisa@gmail.com" ,"ENdnejjd");
