1a5578fa8994c8fa92e587b9a45d6f3d6715ffab
abehforo <arash81.official@gmail.com>
2023-02-28 17:42:54 -0500

    times1

---
+++ DummySQl/time.sql
@@ -0,0 +1,5 @@
+INSERT INTO time VALUES ('12309', 'Monday','8:00 AM', 'True');
+INSERT INTO time VALUES ('12319', 'Monday','8:15 AM', 'True');
+INSERT INTO time VALUES ('12300', 'Monday','8:30 AM', 'True');
+INSERT INTO time VALUES ('33309', 'Monday','8:45 AM', 'True');
+INSERT INTO time VALUES ('17009', 'Monday','9:00 AM', 'True');
