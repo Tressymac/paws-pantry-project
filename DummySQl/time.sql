f79db63b9a2afa069093eb3516e7f6a275542452
sheylatran <sheyla.tran@gmail.com>
2023-02-28 17:47:18 -0500

    tuesday time tables

--- DummySQl/time.sql
+++ DummySQl/time.sql
@@ -3,3 +3,9 @@ INSERT INTO time VALUES ('12319', 'Monday','8:15 AM', 'True');
 INSERT INTO time VALUES ('12300', 'Monday','8:30 AM', 'True');
 INSERT INTO time VALUES ('33309', 'Monday','8:45 AM', 'True');
 INSERT INTO time VALUES ('17009', 'Monday','9:00 AM', 'True');
+
+INSERT INTO time VALUES ('23456', 'Tuesday','9:00 AM', 'True');
+INSERT INTO time VALUES ('29580', 'Tuesday','9:15 AM', 'True');
+INSERT INTO time VALUES ('27439', 'Tuesday','9:30 AM', 'True');
+INSERT INTO time VALUES ('29583', 'Tuesday','9:45 AM', 'True');
+INSERT INTO time VALUES ('21490', 'Tuesday','10:00 AM', 'True');
