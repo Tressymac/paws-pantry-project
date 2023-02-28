c1512bcbc6b9a1376d54cb2e291c257c602c6e76
Jessica <jessicamac48@yahoo.com>
2023-02-28 17:27:40 -0500

    creating api

--- API/routes/api/v1/index.js
+++ API/routes/api/v1/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+import mysql2 from 'mysql2';
+// import express from 'express';
+var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+const connection = mysql2.createConnection({
+    host: "localhost",
+    database: "patientData", 
+    user: "root", 
+    password: "Nihactive"
+})
+
+const app = express();
+
+const PORT = 3306; 
+app.listen(PORT, () => {
+    console.log(`Server : http://localhost:${PORT}`)
+    connection.connect((err) => {
+        if(err) throw err; 
+        console.log("The database is connected");
+    })
+});
+
+app.get('/students', async function (req, res, next) {
+    const sql_query = `SELECT * FROM students;`
+    connection.query(sql_query, (err, results) => {
+        if(err) throw err; 
+        res.json(results);
+    });
 });
 
 module.exports = router;
