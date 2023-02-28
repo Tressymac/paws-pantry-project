bf8cd07e861782a5cfc5dc70ed394e9f14812994
Jessica <jessicamac48@yahoo.com>
2023-02-28 17:35:33 -0500

    fixing routes

--- API/app.js
+++ API/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/api/v1/index');
 
 var app = express();
 
