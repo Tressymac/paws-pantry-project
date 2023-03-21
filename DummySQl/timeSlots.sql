2e5fde4955d88f876cbc4eaa85adbe444854eb35
camoo923 <camoo923@yahoo.com>
2023-03-21 16:53:07 -0400

    Changed 'True' to true in timeSlots.sql

---
+++ DummySQl/timeSlots.sql
@@ -0,0 +1,29 @@
+INSERT INTO timeSlots VALUES ('12309', 'Monday','8:00 AM', true);
+INSERT INTO timeSlots VALUES ('12319', 'Monday','8:15 AM', true);
+INSERT INTO timeSlots VALUES ('12300', 'Monday','8:30 AM', true);
+INSERT INTO timeSlots VALUES ('33309', 'Monday','8:45 AM', true);
+INSERT INTO timeSlots VALUES ('17009', 'Monday','9:00 AM', true);
+
+INSERT INTO timeSlots VALUES ('23456', 'Tuesday','9:00 AM', true);
+INSERT INTO timeSlots VALUES ('29580', 'Tuesday','9:15 AM', true);
+INSERT INTO timeSlots VALUES ('27439', 'Tuesday','9:30 AM', true);
+INSERT INTO timeSlots VALUES ('29583', 'Tuesday','9:45 AM', true);
+INSERT INTO timeSlots VALUES ('21490', 'Tuesday','10:00 AM', true);
+
+INSERT INTO timeSlots VALUES ('34801', 'Wednesday','9:00 AM', true);
+INSERT INTO timeSlots VALUES ('37821', 'Wednesday','11:15 AM', true);
+INSERT INTO timeSlots VALUES ('23870', 'Wednesday','12:30 AM', true);
+INSERT INTO timeSlots VALUES ('35873', 'Wednesday','10:45 AM', true);
+INSERT INTO timeSlots VALUES ('13487', 'Wednesday','8:30 AM', true);
+
+INSERT INTO timeSlots VALUES ('18450', 'Thursday','10:00 AM', true);
+INSERT INTO timeSlots VALUES ('85039', 'Thursday','12:15 AM', true);
+INSERT INTO timeSlots VALUES ('24958', 'Thursday','11:30 AM', true);
+INSERT INTO timeSlots VALUES ('09759', 'Thursday','8:45 AM', true);
+INSERT INTO timeSlots VALUES ('23853', 'Thursday','9:00 AM', true);
+
+INSERT INTO timeSlots VALUES ('23810', 'Friday','11:00 AM', true);
+INSERT INTO timeSlots VALUES ('67483', 'Friday','8:15 AM', true);
+INSERT INTO timeSlots VALUES ('48592', 'Friday','10:30 AM', true);
+INSERT INTO timeSlots VALUES ('89421', 'Friday','12:45 AM', true);
+INSERT INTO timeSlots VALUES ('18475', 'Friday','9:15 AM', true);
